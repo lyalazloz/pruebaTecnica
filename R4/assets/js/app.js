5da561eebbc1d3e5329dada815fea6ddc0aaa7dd
lyalazloz <lyalazloz@gmail.com>
2021-05-26 12:32:29 -0300

    R4, formula get multiples queries

--- R4/assets/js/app.js
+++ R4/assets/js/app.js
@@ -1,10 +1,11 @@
-// import {ajax} from "./Funciones.js";
-// api key para clima 23edd9aff12a611640b6b0eb81d232f6
+let queries= [ `Argentina`, `Colombia`, `Peru`, `Venezuela`]
 
-let params = new URLSearchParams ({
-    access_key: `23edd9aff12a611640b6b0eb81d232f6`,
-    query : `Argentina`,
-    
+let current = queries.map(function(q){
+    console.log(q)
+    let params = new URLSearchParams ({
+        access_key: `23edd9aff12a611640b6b0eb81d232f6`,             
+    })
+    params.append("query", q)
+    fetch(`http://api.weatherstack.com/current?&${params}`).then(res => res.json()).then(console.log);
+    console.log(q)
 })
-  
-fetch(`http://api.weatherstack.com/current?&${params}`).then(res => res.json()).then(console.log);
